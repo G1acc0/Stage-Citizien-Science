a19be7c354b6456c48b88dbcfc0ac0dee03ddc6f
G1acc0 <145464925+G1acc0@users.noreply.github.com>
2025-07-07 11:14:03 +0200

    Backend_Citizien_Science.py

--- Backend_Citizien_Science.py
+++ Backend_Citizien_Science.py
@@ -1,27 +1,80 @@
 from flask import Flask, request, jsonify
-
+import mysql.connector
+import json
 app= Flask(__name__)
-dati=[{"id": "1235","timestamp": "12:21", "num": 30},
-      {"id": "1235","timestamp": "12:21", "num": 30},
-      {"id": "1235","timestamp": "12:21", "num": 30},
-      {"id": "1235","timestamp": "12:21", "num": 30},
-      {"id": "1235","timestamp": "12:21", "num": 30},
-      {"id": "1235","timestamp": "12:21", "num": 30},
-      {"id": "1235","timestamp": "12:21", "num": 30}]
+data= mysql.connector.connect(
+     host="localhost",
+  user="root",
+  password="Giaco",
+  database="stage"
+)
 
 @app.route('/restituisce', methods=['GET'])
 def res():
-   return jsonify(dati)
+    cursor = data.cursor()
+    cursor.execute("SELECT * FROM specifiche")
+    results = cursor.fetchall()
+
+    return jsonify(results)
+  
+    
+        
+               
+       
+   
+       
+    
+    
+   
+   
 
 @app.route('/riceve', methods=['POST'])
 def riceve():
     
     if request.is_json:
-        data = request.get_json()
-        dati.append(data)
-        return jsonify(data), 201
+        ric= request.get_json()
+        
+        val1=ric["ID"]
+        val2=ric["Tempo"]
+        val3=ric["Inquinanti"]
+        cursor = data.cursor()
+        query = "INSERT INTO specifiche (ID, Tempo, Inquinanti) VALUES (%s, %s, %s)"
+        cursor.execute(query, (val1, val2, val3))
+        data.commit()
+        return jsonify({"message": "Dati inseriti con successo"}), 201
+        
     else:
-         return jsonify({"error": "Il corpo della richiesta non è JSON"}), 400
+        return jsonify({"error": "Il corpo della richiesta non è JSON"}), 400
+@app.route('/taglia', methods=['DELETE'])  
+def taglia():
+    ric= request.get_json()
+    cursor = data.cursor()
+    
+    cursor.exceute ("SELECT * FROM specifiche WHERE ID = %s", (ric["ID"]))
+    result = cursor.fetchone()
+    if result is None:
+        return jsonify({"error": "ID non trovato"}), 404
+    else:
+        cursor.execute("DELETE FROM specifiche WHERE ID = %s", (ric["ID"],))
+        data.commit()
+        return jsonify({"message": "Dati eliminati con successo"}), 200
+@app.route('/modifica', methods=['PUT'])
+def modifica():
+    ric = request.get_json()
+    cursor = data.cursor()
+    
+    cursor.execute("SELECT * FROM specifiche WHERE ID = %s", (ric["ID"],))
+    result = cursor.fetchone()
+    
+    if result is None:
+        return jsonify({"error": "ID non trovato"}), 404
+    else:
+        query = "UPDATE specifiche SET Tempo = %s, Inquinanti = %s WHERE ID = %s"
+        cursor.execute(query, (ric["Tempo"], ric["Inquinanti"], ric["ID"]))
+        data.commit()
+        return jsonify({"message": "Dati aggiornati con successo"}), 200
+
+
 
 if __name__ == '__main__':
    app.run( debug=True, host='0.0.0.0', port=5000)
